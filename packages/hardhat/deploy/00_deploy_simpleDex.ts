0c48b997424c258941015f47ee6527d339c6dbee
Mirta <mirta.wolfsdorf@gmail.com>
2024-12-28 00:47:43 +0000

    segundo commit

--- packages/hardhat/deploy/00_deploy_simpleDex.ts
+++ packages/hardhat/deploy/00_deploy_simpleDex.ts
@@ -22,10 +22,19 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("TokenA", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: [],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+  await deploy("TokenB", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -33,12 +42,24 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  const tokenA = await hre.ethers.getContract<Contract>("TokenA", deployer);
+  const tokenB = await hre.ethers.getContract<Contract>("TokenB", deployer);
+  await deploy("SimpleDEX", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [await tokenA.getAddress(), await tokenB.getAddress()],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  const simpleDex = await hre.ethers.getContract<Contract>("SimpleDEX", deployer);
+  console.log("Ya deployamos el SimpleDex este es el address: "+ await simpleDex.getAddress())
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployYourContract.tags = ["SimpleDex"];
